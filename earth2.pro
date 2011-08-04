9b85326bb8a50704e7aa030e04481a4ec84951a9
Skander Jabouzi <jabouzi@gmail.com>
2011-08-04 18:28:40 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/earth2/trunk@2 12f3717e-cbc8-4907-96e5-68cb8dd5a1e9

---
+++ earth2.pro
@@ -0,0 +1,5 @@
+QT += opengl
+TARGET = earth2
+TEMPLATE = app
+SOURCES += main.cpp glwidget.cpp Bmp.cpp
+HEADERS += glwidget.h Bmp.h
---
+++ glwidget.h
@@ -0,0 +1,69 @@
+/*
+ * This code was created by Jeff Molofee 1999
+ * And ported to C++/Qt4 by Wesley Stessens 2009
+ *
+ * Contact Jeff through his website: http://nehe.gamedev.net/
+ * Contact Wesley for port-specific comments: wesley@ubuntu.com
+ */
+
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QGLWidget>
+#include "Bmp.h"
+
+class GLWidget : public QGLWidget {
+    Q_OBJECT
+
+public:
+    GLWidget();
+    ~GLWidget();
+
+protected:
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
+    //void keyPressEvent(QKeyEvent *event);
+    //void changeEvent(QEvent *event);
+    void setCamera(float posX, float posY, float posZ, float targetX, float targetY, float targetZ);
+    void setMouseLeft(bool flag) { mouseLeftDown = flag; };
+    void setMouseRight(bool flag) { mouseRightDown = flag; };
+    void setMousePosition(int x, int y) { mouseX = x; mouseY = y; };
+    void setDrawMode(int mode);
+    void animate(bool flag) { animateFlag = flag; };
+
+    void rotateCamera(int x, int y);
+    void zoomCamera(int dist);
+
+    void setBackgroundRed(float value);             // change background colour
+    void setBackgroundGreen(float value);
+    void setBackgroundBlue(float value);
+
+private:
+    // member functions
+    void initLights();                              // add a white light ti scene
+    unsigned int createEarthDL();
+    unsigned int loadTextureBmp(const char *filename);
+
+    // members
+    int windowWidth;
+    int windowHeight;
+    bool animateFlag;                               // on/off animation
+    bool changeDrawMode;
+    int drawMode;
+    unsigned int listId;                            // display list ID
+    bool mouseLeftDown;
+    bool mouseRightDown;
+    int mouseX;
+    int mouseY;
+    float cameraAngleX;
+    float cameraAngleY;
+    float cameraDistance;
+    float bgColor[4];
+    bool bgFlag;
+    unsigned char* frameBuffer;                     // framebuffer to store RGBA color
+    int bufferSize;
+    void timerEvent(QTimerEvent *event);
+};
+
+#endif // GLWIDGET_H
